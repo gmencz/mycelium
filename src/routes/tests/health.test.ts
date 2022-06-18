4ff0f1d425dce9fc25f6638277ccda361505f026
Gabriel Méndez <yo@gabrielmendezc.com>
2022-06-18 15:32:58 +0200

    health test

--- src/routes/tests/health.test.ts
+++ src/routes/tests/health.test.ts
@@ -13,7 +13,7 @@ describe("/health", () => {
     await teardown(ctx);
   });
 
-  it("should return 200", async () => {
+  it("should return 200 if everything is okay", async () => {
     const response = await ctx.server.inject({
       method: "GET",
       url: "/health",
@@ -22,9 +22,48 @@ describe("/health", () => {
       },
     });
 
-    console.log("status code: ", response.statusCode);
-    console.log("body: ", response.body);
-
     expect(response.statusCode).toBe(200);
+  });
+
+  it("should return 401 to unauthenticated requests", async () => {
+    const response1 = await ctx.server.inject({
+      method: "GET",
+      url: "/health",
+    });
+
+    expect(response1.statusCode).toBe(401);
+
+    const response2 = await ctx.server.inject({
+      method: "GET",
+      url: "/health",
+      headers: {
+        authorization: "Bearer invalid-token",
+      },
+    });
+
+    expect(response2.statusCode).toBe(401);
+  });
+
+  it("should show what's causing the server to be unhealthy", async () => {
+    // Close the NATS connection on purpose.
+    await ctx.nc.close();
+
+    const response = await ctx.server.inject({
+      method: "GET",
+      url: "/health",
+      headers: {
+        authorization: `Bearer ${process.env.INTERNAL_API_SECRET}`,
+      },
+    });
+
+    expect(response.json()).toMatchInlineSnapshot(`
+      Object {
+        "errors": Array [
+          "NATS connection is closed",
+        ],
+      }
+    `);
+
+    expect(response.statusCode).toBe(500);
   });
 });
